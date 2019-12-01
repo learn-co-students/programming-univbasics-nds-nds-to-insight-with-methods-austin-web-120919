c3748e428ee81241535af3060ed74c49f987af8a
Gavin Whitlock <gavinswhitlock@gmail.com>
2019-12-01 02:09:16 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -13,4 +13,12 @@ end
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
+
+  director_total = 0
+  movie_index = 0
+  while movie_index < director_data[:movies].length do
+    director_total += director_data[:movies][movie_index][:worldwide_gross]
+    movie_index += 1
+  end
+  return director_total
 end
